3bc4d050c4602d8b0e19f0d50b39e445125338c4
fatima saleem <fatimasaleem2812@gmail.com>
2020-06-07 17:10:56 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,147 @@
+
+alert("Error!PLease enter a valid password \n happy coding");
+console.log("Error!PLease enter a valid password \n happy coding");
+
+var username;
+var myname="fatima saleem";
+var message="hello world-_-"
+alert(message);
+alert("my name is "+myname);
+alert("my age  is 19");
+alert("i am a computer science student in bbsul \n and i love to learn");
+
+alert("* \n ** \n *** \n **** \n *****");
+var email="fatimasaleem2812@gmail.com";
+alert("my email address is "+email);
+ var book="A smarter way to learn JavaScript"
+ alert( " i am trying to learn form book "+book)
+ document.write("yah!!i can write html contect through js-_-")
+ var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+ alert(a);
+ document.write("<br>************************** new task*************");
+var age=19;
+alert("my age is "+ age);
+alert("the vivitor vist the page 10 time");
+var birth_year=2001;
+document.write("my birth year is "+birth_year);
+
+var   vistitor_name= "John Doe ordered";
+ var Product_title=" T-shirt(s) " ;
+ var Quantity= 5; 
+ document.write(vistitor_name+" ordered " +Quantity + Product_title +" from XYZ store");
+ chap 4
+var a=4;b=5;c=10;
+// 5 legal &  5 illega
+var name;     var 123name;
+var _name;    var -name;
+var Name;     var @name;
+var name123;  var %name;
+var $name;    var *name;
+
+document.write("<br>************************** new task*************");
+ var a=2;b=3;
+ var c=(a+b);
+document.write("the sum of "+a+" and "+b + "is" +  c );
+var  d=(a-b);
+document.write("<br> the sub of "+a+" and "+b + "is" +  d);
+var e=(a/b);
+document.write(" <br> the division of "+a+" and "+b + "is" +  e);
+var f=(a%b);
+document.write(" <br>the modulus of "+a+" and "+b + "is" +  f);
+document.write("<br>************************** new task*************");
+var  intial_value=5;
+document.write("<br>intial value is "+intial_value);
+intial_value++;
+document.write("<br>intial value after ++ "+intial_value);
+intial_value=intial_value+7;
+document.write("<br>intial value after add 7 is:"+intial_value);
+intial_value--;
+document.write("<br>intial value after decreament"+intial_value);
+intial_value/3;
+document.write("<br>intial value after divide by 3:"+intial_value);
+document.write("<br>************************** new task*************");
+var a=prompt("enter a number"+a);
+
+ document.write("<br>let print the table of number you enter:"+a);
+document.write("<br>"+a+ "*" +"1" +"="+(a*1));
+ document.write("<br>"+a+ "*" +"2" +"="+(a*2));
+ document.write("<br>"+a+ "*" +"3" +"="+(a*3));
+  document.write("<br>"+a+ "*" +"4" +"="+(a*4));
+ document.write("<br>"+a+ "*" +"5" +"="+(a*5));
+ document.write("<br>"+a+ "*" +"6" +"="+(a*6));
+ document.write("<br>"+a+ "*" +"7" +"="+(a*7));
+ document.write("<br>"+a+ "*" +"8" +"="+(a*8));
+document.write("<br>"+a+ "*" +"9" +"="+(a*9));
+document.write("<br>"+a+ "*" +"10" +"="+(a*10));
+
+
+
+
+document.write("<br>************************** new task*************");
+document.write("RESULT<br>");
+var a=10;
+document.write("the value of a is :"+a);
+
+document.write("<br> *********************************** <br>");
+a++;
+document.write("the value of a ++ is :"+a);
+document.write(" <br> the value of a ++ is :"+a);
+document.write("<br>______________________________")
+document.write(" <br> the value of a ++ is :"+a);
+a++;
+document.write(" <br> the value of a ++ is :"+a);
+document.write("<br>______________________________")
+--a;
+document.write(" <br> the value of a-- is :"+a);
+a--;
+document.write(" <br> the value of a is :"+a);
+document.write("<br>next task<br>");
+var a = 2, b = 1;
+document.write("<br> the value of a:"+a);
+document.write("<br> the value of b is :"+b);
+ var result = --a;
+document.write(" <br>the value of a after --a:"+a);
+  --b ;
+  document.write(" <br>the value of a after --b:"+b); 
+  ++b;
+  document.write(" <br>the value of a after ++b:"+b); 
+   b--;
+   document.write(" <br>the value of a after b--:"+b); 
+   document.write("<br>******************************<br>")
+
+
+var a=prompt("enter a number"+a);
+if (a==="int"){
+document.write("<br>let print the table of number you enter:"+a);
+document.write("<br>"+a+ "*" +"1" +"="+(a*1));
+document.write("<br>"+a+ "*" +"2" +"="+(a*2));
+document.write("<br>"+a+ "*" +"3" +"="+(a*3));
+document.write("<br>"+a+ "*" +"4" +"="+(a*4));
+document.write("<br>"+a+ "*" +"5" +"="+(a*5));
+document.write("<br>"+a+ "*" +"6" +"="+(a*6));
+document.write("<br>"+a+ "*" +"7" +"="+(a*7));
+document.write("<br>"+a+ "*" +"8" +"="+(a*8));
+document.write("<br>"+a+ "*" +"9" +"="+(a*9));
+document.write("<br>"+a+ "*" +"10" +"="+(a*10));
+}
+else{
+    
+    document.write("<br>" +"5"+ "*" +"1" +"="+(5*1));
+    document.write("<br>"+"5"+ "*" +"2" +"="+(5*2));
+    document.write("<br>"+"5"+ "*" +"3" +"="+(5*3));
+    document.write("<br>"+"5"+ "*" +"4" +"="+(5*4));
+    document.write("<br>"+"5"+ "*" +"5" +"="+(5*5));
+    document.write("<br>"+"5"+ "*" +"6" +"="+(5*6));
+    document.write("<br>"+"5"+ "*" +"7" +"="+(5*7));
+    document.write("<br>"+"5"+ "*" +"8" +"="+(5*8));
+    document.write("<br>"+"5"+ "*" +"9" +"="+(5*9));
+    document.write("<br>"+"5"+ "*" +"10" +"="+(5*10));
+}
+
+document.write("<br> SUBJECT"+ "TOTAL"+"OBTAINED"+"% <br>");
+var a=prompt("enter your marks in english")
+document.write("<br>"+"ENLISH"+ " total 100" +"your marks"+a+ "your % is"+((a/100)*100));
+var b=prompt("enter your marks in math")
+document.write("<br>"+"MATH"+ " total 100:"+"your marks"+b+ "your % is"+((b/100)*100));
+var c=prompt("enter your marks in urdu")
+document.write("<br>"+"URDU"+ " total 100:  " +"your marks "+c+ "your % is"+((c/100)*100));
